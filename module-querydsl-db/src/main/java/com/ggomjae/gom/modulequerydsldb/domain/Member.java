950bdc80cd07c4a208218ac01a118b04f439284b
ggomjae <ggomjae@naver.com>
2021-04-04 13:07:15 +0900

    Querydsl-DB Member Entity 생성

---
+++ module-querydsl-db/src/main/java/com/ggomjae/gom/modulequerydsldb/domain/Member.java
@@ -0,0 +1,30 @@
+package com.ggomjae.gom.modulequerydsldb.domain;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @Column(name = "member_age")
+    private Integer memberAge;
+
+    @Column(name = "time")
+    private Timestamp time;
+}
