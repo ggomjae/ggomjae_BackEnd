4813cdefb506f4464cf7df1a6c0580e75a5486d6
ggomjae <ggomjae@naver.com>
2021-04-05 21:50:23 +0900

    TimeFinal 생성 및 readme Blog Functional 적용

--- api-basic-management/src/test/java/com/ggomjae/gom/apibasicmanagement/service/MemberServiceTest.java
+++ api-basic-management/src/test/java/com/ggomjae/gom/apibasicmanagement/service/MemberServiceTest.java
@@ -2,20 +2,15 @@ package com.ggomjae.gom.apibasicmanagement.service;
 
 import com.ggomjae.gom.apibasicmanagement.JPARepository.MemberRepository;
 import com.ggomjae.gom.apibasicmanagement.dto.ServiceCreateMemberDto;
-import com.ggomjae.gom.modulequerydsldb.domain.Member;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -35,7 +30,7 @@ class MemberServiceTest {
                 .memberAge(29)
                 .memberName("ggomjae")
                 .build();
-        
+
         memberService.createMemberAfterGetMemberName(serviceCreateMemberDto);
 
         //when
