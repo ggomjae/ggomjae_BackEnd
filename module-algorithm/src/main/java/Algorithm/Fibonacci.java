4599be144539ea734642da9079fe27288077aecc
ggomjae <ggomjae@naver.com>
2021-04-09 22:46:23 +0900

    Fibonacci Java 11 구현

--- module-algorithm/src/main/java/Algorithm/Fibonacci.java
+++ module-algorithm/src/main/java/Algorithm/Fibonacci.java
@@ -1,7 +1,32 @@
 package Algorithm;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
+
+@Slf4j
 public class Fibonacci {
+
     public static void main(String[] args){
-        System.out.println("Test ! ! ! ");
+
+        IntSupplier fib = new IntSupplier() {
+
+            private int previous = 0 ;
+            private int current = 1;
+
+            @Override
+            public int getAsInt() {
+
+                int nextValue = this.previous + this.current;
+                this.previous = this.current;
+                this.current = nextValue;
+
+                log.info("previous : {}", this.previous);
+                log.info("current : {}", this.current);
+                return this.previous + this.current;
+            }
+        };
+        System.out.println(IntStream.generate(fib).limit(3).max().getAsInt());
     }
 }
