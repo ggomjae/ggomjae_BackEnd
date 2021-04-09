9ab2f788db2219cb590c94cc0ee7623edea259cc
ggomjae <ggomjae@naver.com>
2021-04-09 20:40:18 +0900

    Java 8 이상의 Algorithm 구현셋팅 완료

--- api-qb-management/src/main/java/com/ggomjae/gom/apiqbmanagement/ApiQbManagementApplication.java
+++ api-qb-management/src/main/java/com/ggomjae/gom/apiqbmanagement/ApiQbManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiQbManagementApplication {
 
-	// reset 
+	// reset
 	public static void main(String[] args) {
 		SpringApplication.run(ApiQbManagementApplication.class, args);
 	}
---
+++ module-algorithm/src/main/java/Algorithm/Fibonacci.java
@@ -0,0 +1,7 @@
+package Algorithm;
+
+public class Fibonacci {
+    public static void main(String[] args){
+        System.out.println("Test ! ! ! ");
+    }
+}
